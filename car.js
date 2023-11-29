4511a9d554d1f5134b06136ea59f8b0b5b1278b0
kerasjeff <kerasjeff@gmail.com>
2023-11-29 08:32:55 +0530

    commit files

--- car.js
+++ car.js
@@ -2,15 +2,16 @@ class Car {
     constructor(x, y, whichCar) {
       this.x = x;
       this.y = y;
-      this.w = 50;
-      this.h = 50;
-      this.speed = 0;
+      this.w = 3 * 16;
+      this.h = 3 * 16;
+      this.speed = {x, y};
       this.whichCar = whichCar;
       this.angle = 0;
     }
   
-    move(distance) {
-      this.x += distance;
+    move() {
+      this.x += this.speed.x;
+      this.y += this.speed.y;
     }
   
     accelerate(acceleration) {
@@ -27,31 +28,39 @@ class Car {
         this.angle += angle;
     }
 
-    draw(canvasContext, coord) {
+    drawRelative(canvasContext, coords) {
+      canvasContext.save();
+      canvasContext.scale(0.7, 0.7);
+      canvasContext.drawImage(images[1], this.w * this.whichCar, 0, this.w, this.h, 
+        coords.x , coords.y, 
+        images[1].width, images[1].height);
+        canvasContext.restore();
+
+    }
+    draw(canvasContext, camera) {
        // canvasContext.fillStyle = this.color;
 
         // Save the current state of the canvas context
         canvasContext.save();
     
         // Translate and rotate the canvas context to position and rotate the car
-        //canvasContext.translate(this.x, this.y);
+       // if (this.whichCar)
+       //canvasContext.translate(camera.x, camera.y);
+
        // canvasContext.rotate(this.angle);
     
        canvasContext.scale(0.7, 0.7);
        
         // Draw the car (assumed rectangular representation)
        // canvasContext.fillRect(-25, -15, 50, 30);
-        canvasContext.drawImage(images[1], 0, 0, this.w, this.h, coord.x, coord.y, images[1].width, images[1].height);
+        canvasContext.drawImage(images[1], this.w * this.whichCar, 0, this.w, this.h, 
+          this.x - camera.x , this.y - camera.y, 
+          images[1].width, images[1].height);
 
         // Restore the canvas context to its original state
         canvasContext.restore();
       }
-/*
-      draw(canvasContext, cameraX) {
-        canvasContext.drawImage(images[1], 0, 0, 50, 50, this.x, this.y, images[1].width, images[1].height);
 
-      }
-      */
    // draw(canvasContext) {
         /*
       canvasContext.fillStyle = this.color;
@@ -76,10 +85,9 @@ class Car {
         
           if (distanceToTarget > 1) {
             const angle = Math.atan2(dy, dx);
-            const speedX = Math.cos(angle);
-            const speedY = Math.sin(angle);
-            this.move(this.speed + speedX);
-            this.y += this.speed + speedY;
+            this.speed.x = Math.cos(angle);
+            this.speed.y = Math.sin(angle);
+            this.move();
                     // Adjust the angle to point towards the current waypoint
 /*
             const targetAngle = Math.atan2(dy, dx);
