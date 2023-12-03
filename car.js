1d96b621dc3f42cf7b937518e61e1709cabe483e
kerasjeff <kerasjeff@gmail.com>
2023-12-03 19:08:07 +0530

    this is complicated logic play with rotate

--- car.js
+++ car.js
@@ -1,106 +1,216 @@
 class Car {
-    constructor(x, y, whichCar) {
-      this.x = x;
-      this.y = y;
-      this.w = 3 * 16;
-      this.h = 24;
-      this.speed = 0.8;
-      this.whichCar = whichCar;
-      this.angle = 0;
-      this.points = [0, 15, 2];
-      this.currentWaypointIndex = 0;
-    }
-  
-    getNextWaypoint() {
-      return this.points[this.currentWaypointIndex++];
+  constructor(x, y, whichCar) {
+    this.x = x;
+    this.y = y;
+    this.w = 3 * 16;
+    this.h = 24;
+    this.speed = 1.8;
+    this.whichCar = whichCar;
+    this.angle = 0;
+    this.points = [];
+    this.whichPath = [0, 15, 3, 8,10, 9]; 
+    this.currentWaypointIndex = 0;
   }
-    move(distance) {
-      this.y += distance;
+
+  getNextWaypoint() {
+    return this.points[this.currentWaypointIndex++];
+  }
+  move(distance) {
+    this.y += distance;
+  }
+
+  accelerate(acceleration) {
+    this.speed += acceleration;
+  }
+
+  brake(deceleration) {
+    this.speed -= deceleration;
+    if (this.speed < 0) {
+      this.speed = 0;
     }
-  
-    accelerate(acceleration) {
-      this.speed += acceleration;
+  }
+
+  drawRadar(ctx, camera) {
+    const radarLength = 120;
+    const radarAngle = 20; // Beam angle in degrees
+    const numRadarBeams = 5;
+
+    ctx.strokeStyle = '#00f000df';
+    ctx.lineWidth = 0.5;
+
+    for (let i = 0; i < numRadarBeams; i++) {
+      const angleRad = 90 * Math.PI / 180 - Math.PI / 2 + (i - (numRadarBeams - 1) / 2) * (radarAngle * Math.PI / 180);
+      const endX = 2 * this.w + radarLength * Math.cos(angleRad);
+      const endY = this.h + radarLength * Math.sin(angleRad);
+
+      ctx.beginPath();
+      ctx.moveTo(this.w + this.w / 2, this.h);
+      ctx.lineTo(endX, endY);
+      ctx.stroke();
     }
-  
-    brake(deceleration) {
-      this.speed -= deceleration;
-      if (this.speed < 0) {
-        this.speed = 0;
-      }
+  }
+
+  draw(canvasContext, camera) {
+
+    // Save the current state of the canvas context
+    canvasContext.save();
+
+    canvasContext.translate(Math.round((this.x - camera.x)), Math.round((this.y - camera.y)));
+    canvasContext.rotate(this.angle);
+    canvasContext.drawImage(images[1], this.w * this.whichCar, 0, this.w, this.h, this.w / 2, this.h / 2,
+      images[1].width / 2, images[1].height);
+
+    if (this.whichCar == 0)
+      this.drawRadar(canvasContext, camera);
+
+    // Restore the canvas context to its original state
+    canvasContext.restore();
+
+  }
+
+  setAutopilot(targetX, targetY) {
+    this.autopilot = true;
+    //this.autopilotTarget = { x: targetX, y: targetY };
+    this.autopilotTarget = {
+      x: PathLister[this.whichPath[0]].x + PathLister[this.whichPath[0]].w + 200,
+      y: PathLister[this.whichPath[0]].y + PathLister[this.whichPath[0]].h / 2
+    };
+
+
+    for (var i = 0; i < 2; i++) {
+         this.points.push({x: PathLister[this.whichPath[0]].x,y: 
+          PathLister[this.whichPath[0]].y + PathLister[this.whichPath[0]].h/2});
     }
 
-    draw(canvasContext, camera) {
-       // canvasContext.fillStyle = this.color;
 
-        // Save the current state of the canvas context
-        canvasContext.save();
+  }
 
-	canvasContext.translate(Math.round((this.x-camera.x)), Math.round((this.y-camera.y)));
-        canvasContext.rotate(this.angle);
-        canvasContext.drawImage(images[1], 
-		this.w * this.whichCar, 0, 
-		this.w, this.h, this.w/2, this.h/2, 
-	images[1].width/2, images[1].height);
+  disableAutopilot() {
+    this.autopilot = false;
+    this.autopilotTarget = null;
+  }
 
-        // Restore the canvas context to its original state
-        canvasContext.restore();
-      }
+  autopilotMove() {
+    if (this.autopilot) {
+      const dx = this.autopilotTarget.x - this.x;
+      const dy = this.autopilotTarget.y - this.y;
+      const distanceToTarget = Math.sqrt(dx * dx + dy * dy);
 
-      setAutopilot(targetX, targetY) {
-        this.autopilot = true;
-        //this.autopilotTarget = { x: targetX, y: targetY };
-	this.autopilotTarget = {
-		x: PathLister[this.points[0]].x+PathLister[this.points[0]].w,
-		y: PathLister[this.points[0]].y+PathLister[this.points[0]].h/2
-	};
-      }
-    
-      disableAutopilot() {
-        this.autopilot = false;
-        this.autopilotTarget = null;
-      }
-    
-      autopilotMove() {
-        if (this.autopilot) {
-          const dx = this.autopilotTarget.x - this.x;
-          const dy = this.autopilotTarget.y - this.y;
-          const distanceToTarget = Math.sqrt(dx * dx + dy * dy);
-        
-          if (distanceToTarget > 1) {
-            const angle = Math.atan2(dy, dx);
+      if (distanceToTarget > 1) {
+        const angle = Math.atan2(dy, dx);
 
-	  // Adjust the angle to point towards the current waypoint
-	  //const targetAngle = Math.atan2(dy, dx);
-	  this.x += Math.cos(angle) *  this.speed;
-	  this.move(this.speed  * Math.sin(angle) );
-	  this.angle = angle;
+        // Adjust the angle to point towards the current waypoint
+        //const targetAngle = Math.atan2(dy, dx);
+        this.x += Math.cos(angle) * this.speed;
+        this.move(this.speed * Math.sin(angle));
+        this.angle = angle;
+        //console.log( PathLister[this.points[this.currentWaypointIndex]].angle * Math.PI / 180 - angle);
 
-	  for (const idx in obstacles)
-	  {
-	  const obstacle = obstacles[idx];
-	  if (
-              (this.x - this.w / 2) < (obstacle.x + obstacle.w) &&
-              (this.x + this.w / 2) > obstacle.x &&
-              (this.y - this.h / 2) < (obstacle.y + obstacle.h) &&
-              (this.y + this.h / 2) > obstacle.y
+        for (const idx in obstacles) {
+          const obstacle = obstacles[idx];
+          // this is good shit
+          if (
+            (
+            (this.x - this.w / 2) < (obstacle.x + obstacle.w) &&
+            (this.x + 3 * this.w / 2) > obstacle.x &&
+            (this.y - this.h / 2) < (obstacle.y + obstacle.h) &&
+            (this.y + 3 * this.h / 2) > obstacle.y
+            )
+            || (
+            (this.x -  3 * this.w / 2) < (obstacle.x + obstacle.w) &&
+            (this.x + this.w / 2) > obstacle.x && 
+            (this.y - 3 * this.h / 2) < (obstacle.y + obstacle.h) &&
+            (this.y +  this.h / 2) > obstacle.y
+            )
           ) {
-                    // Collision detected, adjust movement
-              this.x -= this.speed * Math.cos(angle) +1;
-              this.y += this.speed * Math.sin(angle)+1;
-          }
-	  }
-
-          } else if (++this.currentWaypointIndex < this.points.length) {
-		  this.autopilotTarget.x = PathLister[this.points[this.currentWaypointIndex]].x+PathLister[this.points[this.currentWaypointIndex]].w *
-			  (1-Math.cos(PathLister[this.points[this.currentWaypointIndex]].angle*Math.PI/180)/2)-this.w;
-		  this.autopilotTarget.y = PathLister[this.points[this.currentWaypointIndex]].y+PathLister[this.points[this.currentWaypointIndex]].h *
-			  (1-Math.sin(PathLister[this.points[this.currentWaypointIndex]].angle*Math.PI/180)/2)-this.h;
-
-	} else {
-            this.disableAutopilot();
+            console.log("helo");
+            // Collision detected, adjust movement
+            this.x += this.speed * Math.cos(angle);
+            this.y += this.speed * Math.sin(angle);
           }
         }
+
+
+        for (const idx in global_obstacles) {
+          const obstacle = global_obstacles[idx];
+          if (
+            (this.x - this.w / 2) < (obstacle[0] + obstacle[2]) &&
+            (this.x + 3 * this.w / 2) > obstacle[0] &&
+            (this.y - this.h / 2) < (obstacle[1] + obstacle[3]) &&
+            (this.y + 3 * this.h / 2) > obstacle[1]
+          ) {
+            // Collision detected, adjust movement
+            this.x -= this.speed * Math.cos(angle);
+            this.y -= this.speed * Math.sin(angle);
+          }
+        }
+
+        /*
+        const compareval = { x: undefined, y: undefined};
+        const rad = PathLister[this.points[this.currentWaypointIndex]].angle* Math.PI / 180;
+        const constval = 5;
+        if (Math.cos(rad) == 1 || Math.cos(rad) == -1) {
+          compareval.x = PathLister[this.points[this.currentWaypointIndex]].x
+          if (this.x < compareval.x)
+          {
+            this.x = PathLister[this.points[this.currentWaypointIndex]].x + constval;
+          }
+          compareval.x += PathLister[this.points[this.currentWaypointIndex]].w;
+          if (this.x > compareval.x)
+          {
+            this.x = compareval.x - constval;
+
+
+        } else {
+          compareval.y = PathLister[this.points[this.currentWaypointIndex]].y;
+          if (this.y < compareval.y)
+          {
+            this.y = compareval.y + constval;
+          }
+          compareval.y += PathLister[this.points[this.currentWaypointIndex]].h;
+          if (this.y > compareval.y)
+          {
+            this.y = compareval.y - constval;
+          }
+        }
+        /*
+        console.log(PathLister[this.points[this.currentWaypointIndex]].x + PathLister[this.points[this.currentWaypointIndex]].w *
+          (Math.cos(PathLister[this.points[this.currentWaypointIndex]].angle * Math.PI / 180) / 2) - this.w,
+            PathLister[this.points[this.currentWaypointIndex]].y + PathLister[this.points[this.currentWaypointIndex]].h *
+          (Math.sin(PathLister[this.points[this.currentWaypointIndex]].angle * Math.PI / 180) / 2) - this.h);
+          */
+
+          /*
+        if ((this.x-this.w/2) < PathLister[this.points[this.currentWaypointIndex]].x)
+          this.x = PathLister[this.points[this.currentWaypointIndex]].x + this.w/2;
+        */
+      } else if (this.currentWaypointIndex < this.points.length) {
+        console.log("fuck");
+        this.autopilotTarget = this.points[this.currentWaypointIndex++];
+        /*
+        const angle = PathLister[this.points[this.currentWaypointIndex]].angle;
+        const rad = angle * Math.PI / 180;
+        //console.log(Math.cos(rad),Math.sin(rad));
+
+        console.log(angle, this.angle/Math.PI * 180);
+        /*
+        if (Math.cos(rad) == 1 || Math.cos(rad) == -1) {
+          this.autopilotTarget.x = PathLister[this.points[this.currentWaypointIndex]].x+ PathLister[this.points[this.currentWaypointIndex]].w * Math.cos(rad);
+          this.autopilotTarget.y = PathLister[this.points[this.currentWaypointIndex]].y + PathLister[this.points[this.currentWaypointIndex]].h/2;
+        }
+        else //else if (Math.sin(rad) == 1 || Math.sin(rad) == -1)
+        {
+          this.autopilotTarget.x = PathLister[this.points[this.currentWaypointIndex]].x + PathLister[this.points[this.currentWaypointIndex]].w/2;
+          this.autopilotTarget.y = PathLister[this.points[this.currentWaypointIndex]].y + PathLister[this.points[this.currentWaypointIndex]].h* Math.sin(rad);
+        }
+          */
+
+      } 
+      else {
+        this.disableAutopilot();
       }
-    
+    }
   }
-  
+
+}
+
--- main.js
+++ main.js
@@ -6,70 +6,137 @@ const mapCanvasCtx = mapcanvas.getContext("2d");
 var images = [
     "images/untitled.png",
     "images/cars.png"
-  ].map(function(i) {
+].map(function (i) {
     var img = new Image();
     img.src = i;
     return img;
 });
 
+
+let global_obstacles = [
+    [0, 0, 366, 237],
+    [0, 450, 363,248],
+    [540,477,325,146],
+    [534,0,291,243],
+    [718,237,177,146],
+    [539,699,99, 197],
+    [735,698,95,196],
+    [0,895,366,61]
+];
+
 let PathLister = [
-    new Path(0,242,372,72,0,[15]),           // 0
-    new Path(0,372,372,72,0,[15]),           // 1
-    new Path(370,0,172,226,90,[15]),         // 2
-    new Path(370,480,172,142,90,[15,8,4]),       // 3
-    new Path(378,704,164,110,90,[3,8,6,5]),       // 4
-    new Path(158,822,222,68,0),         // 5
-    new Path(370,896,174,66,90),         // 6
-    new Path(544,894,90,62,0),            // 7
-    new Path(544,620,90,80,0),            // 8
-    new Path(640,702,92,190,90),           // 9
-    new Path(644,620,98,80,0),            // 10
-    new Path(734,620,128,78,0),           // 11
-    new Path(734,894,106,64,0),           // 12
-    new Path(834,704,126,254,90),          // 13
-    new Path(862,476,94,144,90),           // 14
-    new Path(368,222,364,252,0),          // 15
-    new Path(834,0,124,402,0,[0,1,2,3])             // 16
+    new Path(0, 242, 372, 72, 0, [15]),           // 0
+    new Path(0, 372, 372, 72, 0, [15]),           // 1
+    new Path(370, 0, 172, 226, 90, [15]),         // 2
+    new Path(370, 480, 172, 142, 90, [15, 8, 4]),       // 3
+    new Path(378, 704, 164, 110, 90, [3, 8, 6, 5]),       // 4
+    new Path(158, 822, 222, 68, 0,[4,6]),         // 5
+    new Path(370, 896, 174, 66, 90,[7,4,5]),         // 6
+    new Path(544, 894, 90, 62, 0,[6,9,12]),            // 7
+    new Path(544, 620, 90, 80, 180,[3,4,10]),            // 8
+    new Path(640, 702, 92, 190, 90,[10,7,12]),           // 9
+    new Path(644, 620, 98, 80, 180,[11,8,9]),            // 10
+    new Path(734, 620, 128, 78, 0),           // 11
+    new Path(734, 894, 106, 64, 0),           // 12
+    new Path(834, 704, 126, 254, 90),          // 13
+    new Path(862, 476, 94, 144, 90),           // 14
+    new Path(368, 222, 364, 252, 0,[0, 1, 2, 3] ),          // 15
+    new Path(834, 0, 124, 402, 0)             // 16
 ];
 
 let obstacles;
 
+function checkCollision(a, b) {
+    //The sides of the rectangles
+    let leftA, leftB;
+    let rightA, rightB;
+    let topA, topB;
+    let bottomA, bottomB;
+
+    //Calculate the sides of rect A
+    leftA = a.x;
+    rightA = a.x + a.w;
+    topA = a.y;
+    bottomA = a.y + a.h;
+
+    //Calculate the sides of rect B
+    leftB = b.x;
+    rightB = b.x + b.w;
+    topB = b.y;
+    bottomB = b.y + b.h;
+
+    //If any of the sides from A are outside of B
+    if( bottomA <= topB )
+    {
+        return false;
+    }
+
+    if( topA >= bottomB )
+    {
+        return false;
+    }
+
+    if( rightA <= leftB )
+    {
+        return false;
+    }
+
+    if( leftA >= rightB )
+    {
+        return false;
+    }
+
+    //If none of the sides from A are outside B
+    return true;
+}
+
 async function main() {
-    await Promise.all(images.map(function(image) {
-        return new Promise(function(resolve, reject) {image.onload = resolve;});
+    await Promise.all(images.map(function (image) {
+        return new Promise(function (resolve, reject) { image.onload = resolve; });
     }));
     const mapSize = 300;
-   const  ycoord = 240
-    let car = new Car(100,ycoord, 0);
-    obstacles = [new Car( 100, ycoord, 1), new Car( 180, ycoord, 1), new Car( 100, 280, 1)]
+    const ycoord = 240
+    let car = new Car(100, ycoord+32, 0);
+    obstacles = [new Car(100, ycoord, 1), new Car(180, ycoord, 1)]
 
-    let camera = { x: 0, y: 0 , w: images[0].width, h:images[0].height};
+    let camera = { x: 0, y: 0, w: images[0].width, h: images[0].height };
 
 
-    canvasCtx.drawImage(images[0], camera.x, camera.y, canvas.width, canvas.height, 
-    0, 0, camera.w, camera.h);
+    canvasCtx.drawImage(images[0], camera.x, camera.y, canvas.width, canvas.height,
+        0, 0, camera.w, camera.h);
 
-    mapCanvasCtx.drawImage(images[0], camera.x, camera.y, 
-        canvas.width, 
-        canvas.height, 
+    mapCanvasCtx.drawImage(images[0], camera.x, camera.y,
+        canvas.width,
+        canvas.height,
         0, 0, mapSize, mapSize);
     let zoomFactorInt = 1;
-    document.querySelector("#zoom").addEventListener("click", function() {
-        if (zoomFactorInt === 1)  {
+    document.querySelector("#zoom").addEventListener("click", function () {
+        if (zoomFactorInt === 1) {
             zoomFactorInt = 0;
-            mapCanvasCtx.drawImage(images[0], 
-                0, 0, mapSize, mapSize);  
+            mapCanvasCtx.drawImage(images[0],
+                0, 0, mapSize, mapSize);
 
         }
         else {
             zoomFactorInt = 1;
-            mapCanvasCtx.drawImage(images[0], camera.x, camera.y, 
-                canvas.width, canvas.height, 
+            mapCanvasCtx.drawImage(images[0], camera.x, camera.y,
+                canvas.width, canvas.height,
                 0, 0, mapSize, mapSize);
 
         }
     });
-    car.setAutopilot(600,ycoord*2);
+
+ car.setAutopilot();
+    mapcanvas.addEventListener("click", function(e) {
+        if (zoomFactorInt == 1)
+            return;
+        let rect = mapcanvas.getBoundingClientRect();
+        let width = e.clientX-rect.left, height = e.clientY-rect.top;
+        if (width < rect.right && height < rect.right)
+        {
+        }
+           
+    });
 
     document.addEventListener("keydown", (event) => {
         if (event.defaultPrevented) {
@@ -77,79 +144,49 @@ async function main() {
         }
         let moveImg = 20;
         switch (event.key) {
-            case "ArrowDown":
-                /*
-                camera.y += moveImg;
-                if (camera.y >= (camera.h - camera.h/camera.m_const)) {
-                    //camera.y = camera.h - camera.h/camera.m_const;
-                    camera.y -= moveImg;
-                } 
-
-                */
-              break;
-            case "ArrowUp":
-                /*
-                camera.y -= moveImg;
-                if (camera.y < 0) {
-                    camera.y = 0; }
-                    */
-              break;
-            case "ArrowLeft":
-                /*
-                camera.x -= moveImg;
-                if (camera.x < 0) {
-                    camera.x = 0; }    
-                    */    
-                break;
-            case "ArrowRight":
-                /*
-                camera.x += moveImg; 
-                if (camera.x >= (camera.w - camera.w/camera.m_const)) {
-                    camera.x = camera.w - camera.w/camera.m_const; }// images[0].width - moveImg; }
-                break;
-                */
             default:
                 return; // Quit when this doesn't handle the key event.
         }
 
     });
 
-    let othervehiclespeed = {x:undefined, y:undefined};
+    let othervehiclespeed = { x: undefined, y: undefined };
 
     function updateCoord() {
 
         car.autopilotMove();
         //https://lazyfoo.net/tutorials/SDL/39_tiling/index.php
-        camera.x = Math.round((car.x + car.w /2) - canvas.width / 2);
-        camera.y =  Math.round(( car.y + car.h / 2 ) - canvas.height / 2);
+        camera.x = Math.round((car.x + car.w / 2) - canvas.width / 2);
+        camera.y = Math.round((car.y + car.h / 2) - canvas.height / 2);
         // keep camera  in bound
         if (camera.y < 0) {
-            camera.y = 0; 
+            camera.y = 0;
         }
         if (camera.x < 0) {
-                camera.x = 0;
-            }  
+            camera.x = 0;
+        }
 
         if (camera.y >= (camera.h - canvas.height)) {
             camera.y = camera.h - canvas.height;
-        } 
+        }
         if (camera.x >= (camera.w - canvas.width)) {
-            camera.x = camera.w - canvas.width; 
+            camera.x = camera.w - canvas.width;
         }
     }
     function render() {
         updateCoord();
-        canvasCtx.drawImage(images[0],camera.x, camera.y, 
-            camera.w, camera.h, 
+        canvasCtx.drawImage(images[0], camera.x, camera.y,
+            camera.w, camera.h,
             0, 0, camera.w, camera.h);
 
-        if (zoomFactorInt === 1)  {mapCanvasCtx.drawImage(images[0],camera.x,camera.y, 
-            canvas.width, canvas.height, 
-            0, 0, mapSize, mapSize);
+        if (zoomFactorInt === 1) {
+            mapCanvasCtx.drawImage(images[0], camera.x, camera.y,
+                canvas.width, canvas.height,
+                0, 0, mapSize, mapSize);
         }
         else {
-            mapCanvasCtx.drawImage(images[0], 
-                0, 0, mapSize, mapSize);  
+            mapCanvasCtx.drawImage(images[0],
+                0, 0, mapSize, mapSize);
         }
         obstacles.forEach(element => {
             element.draw(canvasCtx, camera);
